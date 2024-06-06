e9bd011c5623a9613128eb2d0b80f4c6478471d4
Lakvindu siriwardena <130139698+Lakvindu05@users.noreply.github.com>
2024-06-06 12:08:02 +0530

    created the container and added the styles to it

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Container = () => {
   return (
-    <div>
-      Container
-    </div>
+    <section className="container">
+      <div className="content">
+        <span id="text1">Let's Build Your Digital Empire Together <br/></span>
+        <span id="text2">Supercharge Your Business With High-Impact Social Media Advertising. <br />
+          Maximise ROI, Skyrocket sales. Unleash Your Business Potential.</span>
+      </div>
+      <img src="/Container-img.png" alt="" />
+    </section>
   )
 }
 
