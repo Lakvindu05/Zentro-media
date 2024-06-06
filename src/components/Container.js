06f8d4c0062fef7b0d28334bd9bbd3f49835811d
Lakvindu siriwardena <130139698+Lakvindu05@users.noreply.github.com>
2024-06-06 15:27:56 +0530

    "Updated App.css and Container.js: changed CSS styles, added new classes and elements, and restructured HTML elements."

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,12 +3,28 @@ import React from 'react'
 const Container = () => {
   return (
     <section className="container">
-      <div className="content">
-        <span id="text1">Let's Build Your Digital Empire Together <br/></span>
-        <span id="text2">Supercharge Your Business With High-Impact Social Media Advertising. <br />
-          Maximise ROI, Skyrocket sales. Unleash Your Business Potential.</span>
+      <div className="content1">
+        <div className="intro">
+          <span id="text1">Let's Build Your Digital Empire Together <br/></span>
+          <span id="text2">Supercharge Your Business With High-Impact Social Media Advertising. <br />
+            Maximise ROI, Skyrocket sales. Unleash Your Business Potential.</span>
+        </div>
+        <img src="/Container-img.png" alt="" />
       </div>
-      <img src="/Container-img.png" alt="" />
+      <div className="content2">
+        <div className="card">
+          <span id="number"> 5 </span>
+          <span id="title">Clients Helped</span>
+        </div>
+        <div className="card">
+          <span id="number"> 2+ </span>
+          <span id="title">Years Experience</span>
+        </div>
+        <div className="card">
+          <span id="number"> 3 </span>
+          <span id="title">Services Offered</span>
+        </div>
+      </div>
     </section>
   )
 }
